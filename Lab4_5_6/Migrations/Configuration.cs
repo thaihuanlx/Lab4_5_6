06dd6cf0109efe057eca12a3a9ff2b2f59154fef
Admin <Admin@DESKTOP-EQ69H0C>
2021-07-01 14:50:34 +0700

    Add project files.

---
+++ Lab4_5_6/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Lab4_5_6.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Lab4_5_6.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Lab4_5_6.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
