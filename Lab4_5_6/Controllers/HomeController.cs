590c2a7e42cb120704fe389a7491cb7bca991fb6
HTH <thaihuanlx@gmail.com>
2021-07-12 17:58:33 +0700

    Change

--- Lab4_5_6/Controllers/HomeController.cs
+++ Lab4_5_6/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using Lab4_5_6.ViewModels;
 
 namespace Lab4_5_6.Controllers
 {
@@ -21,7 +22,12 @@ namespace Lab4_5_6.Controllers
                 .Include(c => c.Lecturer)
                 .Include(c => c.Category)
                 .Where(c => c.DateTime > DateTime.Now);
-            return View(upcomingCourses);
+            var viewModel = new CoursesViewModel
+            {
+                UpcomingCourses = upcomingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
 
         public ActionResult About()
