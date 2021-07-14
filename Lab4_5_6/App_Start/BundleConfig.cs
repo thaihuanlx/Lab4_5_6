7a3e4e572d675007c11773d90800b0de8a6eb891
HTH <thaihuanlx@gmail.com>
2021-07-14 09:09:04 +0700

    Change

--- Lab4_5_6/App_Start/BundleConfig.cs
+++ Lab4_5_6/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace Lab4_5_6
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
                       "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
